cd32d401d098b446c3d49cf91b07d836c9c8e9d2
bstinenbosch <basstinenbosch@msn.com>
2017-01-09 09:19:06 +0100

    Keeping todo's in memory

--- core/js/animations.js
+++ core/js/animations.js
@@ -2,25 +2,79 @@ $(document).ready(function(){
     load();
 });
 
+var todoItems = [];
+
 function load(){
     //load todo's
     var addTodosToList = function (todos) {
-        console.log("Loading todos from server");
-        var todolist = document.getElementById("todo");
-        for (var key in todos) {//add all todosLists
-            var group = document.createElement("section");
-            group.className = "todoList";
-            group.innerHTML +=  "<header><p>" + todos[key].name + "</p><nav><a href=''>&#xf067;</a><a href=''>&#xf0c9;</a></nav></header><ul><li>hello</li></ul>";
-            var list = group.getElementsByTagName("ul")[0];
-            for (var key2 in todos[key].todos) {
-                var todoItem = todos[key].todos[key2];
-                list.innerHTML += "<li>" + todoItem.message + "</li>";
-                list.innerHTML += "<div class='itemInfo'><p>overdue: " + todoItem.deadline + "</p><p>importance 5</p></div>";
+        //check if the todo items are differen
+        var sameTodos = true;
+        if (todoItems.length == 0) {
+            sameTodos = false;
+        } else {
+            loop: for (var key in todos) {
+                for (var key2 in todos[key].todos) {
+                    var todoItem = todos[key].todos[key2];
+                    var timestamp = 0;
+                    //check if item is loaded
+                    for (var key3 in todoItems) {
+                        if (todoItems[key3].id == todoItem.id) {
+                            timestamp = todoItems[key3].timestamp;
+                        }
+                    }
+                    //check if item is up to date
+                    if (todoItem.timestamp != timestamp) {
+                        sameTodos = false;
+                        break loop;
+                    }
+                }
             }
-            todolist.appendChild(group);
+        }
+        if (!sameTodos) {
+            todoItems = [];
+            updateTodos(todos);
+            animateItems();
         }
     };
 
+    setInterval(function () {
+        //  note: to use setInterval() you have to implement a mechanisms
+        //  that checks which todos are already shown to the client
+
+        $.getJSON("/todos", addTodosToList)
+            .error(function (jqXHR, textStatus, errorThrown) {
+                console.log("error " + textStatus);
+                console.log("incoming Text " + jqXHR.responseText);
+            });
+    }, 2000);
+}
+
+/**
+ * Replace all todo's with new todo's
+ * @param todos
+ */
+function updateTodos(todos) {
+    $("#todo").empty();
+    console.log("Loading todos from server");
+    var todolist = document.getElementById("todo");
+    for (var key in todos) {//add all todosLists
+        var group = document.createElement("section");
+        group.className = "todoList";
+        group.innerHTML +=  "<header><p>" + todos[key].name + "</p><nav><a href=''>&#xf067;</a><a href=''>&#xf0c9;</a></nav></header><ul></ul>";
+        var list = group.getElementsByTagName("ul")[0];
+        for (var key2 in todos[key].todos) {
+            var todoItem = todos[key].todos[key2];
+            //add todo to client todo's
+            todoItems.push(todoItem);
+            //add todo to screen
+            list.innerHTML += "<li>" + todoItem.message + "</li>";
+            list.innerHTML += "<div class='itemInfo'><p>overdue: " + todoItem.deadline + "</p><p>importance 5</p></div>";
+        }
+        todolist.appendChild(group);
+    }
+}
+
+function animateItems() {
     //animate todo Items drop down
     $(".todoList > ul > li").click(function(){
         var infoHeight = 50;
@@ -58,15 +112,4 @@ function load(){
             $(this).next().children().css("visibility", "visible");
         }
     })
-
-    setInterval(function () {
-        //  note: to use setInterval() you have to implement a mechanisms
-        //  that checks which todos are already shown to the client
-
-        $.getJSON("/todos", addTodosToList)
-            .error(function (jqXHR, textStatus, errorThrown) {
-                console.log("error " + textStatus);
-                console.log("incoming Text " + jqXHR.responseText);
-            });
-    }, 2000);
 }
